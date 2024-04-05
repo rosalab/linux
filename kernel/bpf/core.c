447e7c6f65d8d3f876dfad76bdfaafe4001654cb
Jinghao Jia <jinghao7@illinois.edu>
2024-04-05 15:41:59 -0500

    bpf,iu: fix NULL-ptr-deref on error path
    Previous implementation of freeing iu programs only handles the normal
    case where a *sucessfully loaded* program loses all its refcnts. The
    jited flag in the program struct is cleared in __bpf_prog_put_noref so
    that bpf_jit_free will not attempt to free some nonexistent memory (iu
    programs are mapped differently anyway)
    
    This, however, causes problems on the error path during program load,
    because the error path does not go through __bpf_prog_put_noref but
    rather calls bpf_jit_free directly. Therefore, it is needed to check
    whether the bpf_prog actually represents an iu prog in bpf_jit_free.
    
    Fixes: 384d394a6145 ("free program memory")
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -907,7 +907,7 @@ void bpf_jit_binary_free(struct bpf_binary_header *hdr)
  */
 void __weak bpf_jit_free(struct bpf_prog *fp)
 {
-	if (fp->jited) {
+	if (fp->jited && !fp->no_bpf) {
 		struct bpf_binary_header *hdr = bpf_jit_binary_hdr(fp);
 
 		bpf_jit_binary_free(hdr);
