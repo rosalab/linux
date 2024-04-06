d0e570527c61eb4cddbba1a0c2495324f9b6ad00
Jinghao Jia <jinghao7@illinois.edu>
2024-04-06 15:57:32 -0500

    samples/bpf: update recursion benchmark
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- samples/bpf/recursive_kern.c
+++ samples/bpf/recursive_kern.c
@@ -1,13 +1,12 @@
 // Referenced from bpf/samples/hello_kern.c
 #include <linux/bpf.h>
+#include <linux/ptrace.h>
 #include <bpf/bpf_helpers.h>
 
-struct {
-	__uint(type, BPF_MAP_TYPE_ARRAY);
-	__type(key, u32);
-	__type(value, u32);
-	__uint(max_entries, 3);
-} data_map SEC(".maps");
+#define noinline __attribute__((__noinline__))
+
+/* accum = state[63:32], n = state[31:0] */
+static __u64 state;
 
 struct {
 	__uint(type, BPF_MAP_TYPE_PROG_ARRAY);
@@ -16,53 +15,42 @@ struct {
 	__uint(max_entries, 1);
 } prog_map SEC(".maps");
 
-
-SEC("tracepoint/syscalls/sys_enter_write")
-int trace_enter_execve(void *ctx)
+static noinline int mid(struct pt_regs *ctx)
 {
-
-	int pid = bpf_get_current_pid_tgid();
-
-	// Checked that it's triggered by our own userspace proc
-	__u32 pid_key = 0;
-	__u32* pid_curr = (__u32*) bpf_map_lookup_elem(&data_map, &pid_key);
-	if(pid_curr == 0 || *pid_curr != pid){
-		return 0;
-	}
-
-	// Get the n and accum
-	__u64 time = bpf_ktime_get_ns();
-	bpf_printk("Time: %ul", time);
-	__u32 n_key = 1;
-	__u32 accum_key = 2;
-	__u32* n_curr = (__u32*) bpf_map_lookup_elem(&data_map, &n_key);
-	if(n_curr == 0){
-		return 0;
-	}
-	__u32* accum_curr = (__u32*) bpf_map_lookup_elem(&data_map, &accum_key);
-	if(accum_curr == 0){
-		return 0;
-	}
-
-	__u32 accum = *accum_curr;
-	__u32 n = *n_curr;
-	bpf_printk("n: %d, accum: %d", n, accum);
-	
-	// Base case
-	if(n == 1){
-		__u64 time = bpf_ktime_get_ns();
-		bpf_printk("Answer: %d, Time: %ul", accum, time);
-		return 0;
-	}
-
-	// Else, update accum and make tail call
-	accum *= n;
-	n -= 1;
-	bpf_map_update_elem(&data_map, &n_key, &n, BPF_ANY);
-	bpf_map_update_elem(&data_map, &accum_key, &accum, BPF_ANY);
 	bpf_tail_call(ctx, &prog_map, 0);
-	// static long (*bpf_tail_call)(void *ctx, void *prog_array_map, __u32 index) = (void *) 12;
+}
 
+SEC("kprobe/")
+int calculate_tail_factorial(struct pt_regs *ctx)
+{
+	/* Get the n and accum */
+	__u32 accum = state >> 32;
+	__u32 n = state & 0xFFFFFFFFULL;
+
+	/* Base case */
+	if(!n)
+		return 0;
+
+	/* Else, update accum and make tail call */
+	accum += n;
+	n -= 1;
+	state = (((__u64)accum) << 32) | n;
+	accum = state >> 32;
+	n = state & 0xFFFFFFFFULL;
+	bpf_tail_call(ctx, &prog_map, 0);
+
+	return 0;
+}
+
+SEC("kprobe/kprobe_target_func")
+int bpf_recursive(struct pt_regs *ctx)
+{
+	/* accum = 0, n = ctx->rdi */
+	state = ctx->di & 0xFFFFFFFFULL;
+	__u64 start = bpf_ktime_get_ns();
+	mid(ctx);
+	__u64 stop = bpf_ktime_get_ns();
+	bpf_printk("Time: %llu\n", stop - start);
 	return 0;
 }
 
--- samples/bpf/recursive_user.c
+++ samples/bpf/recursive_user.c
@@ -10,9 +10,11 @@ int main(int argc, char **argv)
 	struct bpf_link *link = NULL;
 	struct bpf_program *prog;
 	struct bpf_object *obj;
-	struct bpf_map *data, *prog_map;
+	struct bpf_map *prog_map;
+	char buf[128] = { 0 };
 
-	obj = bpf_object__open_file("recursive_kern.o", NULL);
+	snprintf(buf, sizeof(buf), "%s_kern.o", argv[0]);
+	obj = bpf_object__open(buf);
 	if (libbpf_get_error(obj)) {
 		fprintf(stderr, "ERROR: opening BPF object file failed\n");
 		return 0;
@@ -23,40 +25,31 @@ int main(int argc, char **argv)
 		goto cleanup;
 	}
 
-	data = bpf_object__find_map_by_name(obj, "data_map");
-	if (libbpf_get_error(data)) {
-		fprintf(stderr, "ERROR: Could not find map: data_map\n");
-		goto cleanup;
-	}
 	prog_map = bpf_object__find_map_by_name(obj, "prog_map");
 	if (libbpf_get_error(prog_map)) {
 		fprintf(stderr, "ERROR: Could not find map: prog_map\n");
 		goto cleanup;
 	}
-	int data_map_fd = bpf_object__find_map_fd_by_name(obj, "data_map");
+
 	int prog_map_fd = bpf_object__find_map_fd_by_name(obj, "prog_map");
 
-	int pid_key = 0;
-	int n_key = 1;
-	int accum_key = 2;
-
-	int pid = getpid();
-	printf("PID: %d\n", pid);
-	int n_init = 10;
-	int accum_init = 1;
-	bpf_map_update_elem(data_map_fd, &pid_key, &pid, BPF_ANY);
-	bpf_map_update_elem(data_map_fd, &n_key, &n_init, BPF_ANY);
-	bpf_map_update_elem(data_map_fd, &accum_key, &accum_init, BPF_ANY);
-
-	prog = bpf_object__find_program_by_name(obj, "trace_enter_execve");
+	prog = bpf_object__find_program_by_name(obj, "calculate_tail_factorial");
 	if (!prog) {
 		fprintf(stderr, "ERROR: finding a prog in obj file failed\n");
 		goto cleanup;
 	}
-	int prog_fd_index = 0;
+	int prog_fd_idx = 0;
 	int prog_fd = bpf_program__fd(prog);
-	printf("Prog fd: %d\n", prog_fd);
-	bpf_map_update_elem(prog_map_fd, &prog_fd_index, &prog_fd, BPF_ANY);
+	if (bpf_map_update_elem(prog_map_fd, &prog_fd_idx, &prog_fd, BPF_ANY) < 0) {
+		fprintf(stderr, "ERROR: updating prog array failed\n");
+		goto cleanup;
+	}
+
+	prog = bpf_object__find_program_by_name(obj, "bpf_recursive");
+	if (!prog) {
+		fprintf(stderr, "ERROR: finding a prog in obj file failed\n");
+		goto cleanup;
+	}
 
 	link = bpf_program__attach(prog);
 	if (libbpf_get_error(link)) {
@@ -65,11 +58,8 @@ int main(int argc, char **argv)
 		goto cleanup;
 	}
 
-	fprintf(stdout, "Triggered a write syscall\n");
-
 	bpf_link__pin(link, "/sys/fs/bpf/recursive_link");
-
-	scanf("Waiting...");
+	bpf_object__pin(obj, "/sys/fs/bpf/recursive_obj");
 
 cleanup:
 	bpf_link__destroy(link);
