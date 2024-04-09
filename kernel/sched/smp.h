1896952c4339a2b5b3a139e61554a4e94f89434b
Raj <rjsu26@gmail.com>
2024-04-09 01:05:12 -0400

    rex-termination: Read per-cpu termination flag before changing execution to panic handler

--- kernel/sched/smp.h
+++ kernel/sched/smp.h
@@ -9,7 +9,7 @@ extern void sched_ttwu_pending(void *arg);
 extern bool call_function_single_prep_ipi(int cpu);
 
 #ifdef CONFIG_SMP
-extern void flush_smp_call_function_queue(void);
+extern void flush_smp_call_function_queue(struct pt_regs *regs);
 #else
-static inline void flush_smp_call_function_queue(void) { }
+static inline void flush_smp_call_function_queue(struct pt_regs *regs) { }
 #endif
