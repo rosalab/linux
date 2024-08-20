20a041b18781397f48d08dde54c53fcd301ef9bd
Jinghao Jia <jinghao7@illinois.edu>
2024-08-19 21:16:05 -0500

    bpf,rex: remove bpf_trace_printk_rex
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -392,45 +392,6 @@ static const struct bpf_func_proto bpf_trace_printk_proto = {
 	.arg2_type	= ARG_CONST_SIZE,
 };
 
-static DEFINE_RAW_SPINLOCK(trace_printk_lock);
-
-// proto not needed since it does not go through the verifier
-BPF_CALL_5(bpf_trace_printk_rex, char *, fmt, u32, fmt_size, u64, arg1,
-		u64, arg2, u64, arg3)
-{
-	u64 args[MAX_TRACE_PRINTK_VARARGS] = { arg1, arg2, arg3 };
-	static char prep[BPF_TRACE_PRINTK_SIZE];
-	struct bpf_bprintf_data data = {
-		.get_bin_args	= true,
-		.get_buf	= true,
-	};
-	unsigned long flags;
-	int ret;
-
-	raw_spin_lock_irqsave(&trace_printk_lock, flags);
-
-	// Assume Rust program sends exactly the &str length
-	if (fmt_size >= BPF_TRACE_PRINTK_SIZE)
-		return -EINVAL;
-
-	memcpy(prep, fmt, fmt_size);
-	prep[fmt_size] = '\0';
-
-	ret = bpf_bprintf_prepare(prep, fmt_size + 1, args,
-				  MAX_TRACE_PRINTK_VARARGS, &data);
-	if (ret < 0)
-		return ret;
-
-	ret = bstr_printf(data.buf, MAX_BPRINTF_BUF, prep, data.bin_args);
-
-	trace_bpf_trace_printk(data.buf);
-	raw_spin_unlock_irqrestore(&trace_printk_lock, flags);
-
-	bpf_bprintf_cleanup(&data);
-
-	return ret;
-}
-
 static void __set_printk_clr_event(void)
 {
 	/*
