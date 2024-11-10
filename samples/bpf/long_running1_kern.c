11915151c361acf906f60ef98298957875ff5df1
Eric Schneider <ericts@vt.edu>
2024-11-10 02:21:17 +0000

    i hope this works lol

---
+++ samples/bpf/long_running1_kern.c
@@ -0,0 +1,37 @@
+#include <linux/ptrace.h>
+#include <linux/version.h>
+#include <uapi/linux/bpf.h>
+#include <uapi/linux/in6.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
+#include "trace_common.h"
+
+#if defined(CONFIG_FUNCTION_TRACER)
+#define CC_USING_FENTRY
+#endif
+
+#define MAX_DICT_SIZE 1000 
+#define MAX_DICT_VAL  10000
+
+struct 
+{
+    __uint(type, BPF_MAP_TYPE_LRU_HASH);
+    __uint(max_entries, MAX_DICT_SIZE);
+    __type(key, int);
+    __type(value,int);
+} 
+my_map SEC(".maps");
+
+void populate_map()
+{
+	bpf_printk("Hello world ;)\n");
+}
+
+SEC("tracepoint/syscalls/sys_exit_saterm_test")
+int main(struct pt_regs *ctx)
+{
+	populate_map();
+	return 0;
+}
+
